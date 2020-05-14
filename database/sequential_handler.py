2ad6a0bd85a1f055d2e1ff37d2b4fa2a1306a19b
Aleksandar <aleksandar.tadic98@gmail.com>
2020-05-15 01:57:25 +0200

    master

---
+++ database/sequential_handler.py
@@ -0,0 +1,122 @@
+from database.data_handler import DataHandler
+import json
+import pickle
+
+
+class SequentialHandler(DataHandler):
+    def __init__(self, filepath, meta_filepath):
+        super().__init__()
+        self.filepath = "database/data/" + filepath
+        self.meta_filepath = "database/metadata/" + meta_filepath
+        self.data = []
+        self.metadata = {}
+        self.load_data()
+
+    def load_data(self):
+        try:
+            with open(self.filepath, "rb") as dfile:
+                self.data = pickle.load(dfile)
+        except FileNotFoundError:
+            print(self.filepath)
+            print("File nije pronadjen!")
+            self.save_data()
+            print("File kreiran!")
+        
+        try:
+            with open(self.meta_filepath, "rb") as meta_file:
+                self.metadata = json.load(meta_file)
+        except FileNotFoundError:
+            print(self.meta_filepath)
+            print("Meta file nije pronadjen!")
+
+    def save_data(self):
+        with open(self.filepath, "wb") as f:
+            pickle.dump(self.data, f)
+
+    def get_one(self, id):
+        temp_object = self.data[self.binary_search(id)]
+        if temp_object is not None:
+            return temp_object
+        else:
+            print("Objekat nije pronadjen!")
+
+    def get_all(self):
+        return self.data
+
+    def insert(self, obj):
+        location = self.find_location_binary(obj)
+        if (location == None):
+            self.data.append(obj)
+        else:
+            self.data.insert(location, obj)
+        self.save_data()
+
+    def insert_many(self, obj_list):
+        if len(obj_list) > 0:
+            if not isinstance(obj_list, list):
+                return
+            for obj in obj_list:
+                self.insert(obj)
+
+    # def edit(self, id, col, value):
+    #     change_data = self.data[self.binary_search(id)]
+    #     setattr(change_data, self.metadata["collumns"][col], value)
+    #     # self.save_data()
+
+    def edit(self, id, attr, value):
+        change_data = self.data[self.binary_search(id)]
+        setattr(change_data, attr, value)
+        self.save_data()
+
+    def delete_one(self, id):
+        temp_object = self.data[self.binary_search(id)]
+        if temp_object is not None:
+            self.data.remove(temp_object)
+            self.save_data()
+  
+    def binary_search(self, id):
+        start = 0
+        end = len(self.data)-1
+        found = False
+        while (start <= end and not found):
+            mid = start + (end - start)//2
+            if getattr(self.data[mid], (self.metadata["key"])) == getattr(id, (self.metadata["key"])):
+                found == True
+                return mid
+            elif getattr(self.data[mid], (self.metadata["key"])) < getattr(id, (self.metadata["key"])):
+                start = mid + 1
+            else:
+                end = mid - 1
+        return None
+
+    # def find_location_binary(self, obj):
+    #     start = 0
+    #     end = len(self.data)-1
+    #     while start <= end:
+    #         mid = start + (end - start)//2
+    #         if getattr(self.data[start], (self.metadata["key"])) > getattr(obj, (self.metadata["key"])):
+    #             return start
+    #         elif getattr(self.data[mid], (self.metadata["key"])) > getattr(obj, (self.metadata["key"])):
+    #             return mid
+    #         elif getattr(self.data[mid], (self.metadata["key"])) < getattr(obj, (self.metadata["key"])):
+    #             start = mid + 1
+    #         else:
+    #             end = mid - 1
+    #     return None
+
+    def find_location_binary(self, obj):
+        start = 0
+        end = len(self.data)-1
+        while start <= end:
+            mid = start + (end - start)//2
+            if getattr(self.data[mid], (self.metadata["key"])) > getattr(obj, (self.metadata["key"])) and getattr(self.data[mid-1], (self.metadata["key"])) < getattr(obj, (self.metadata["key"])):
+                return mid
+            elif getattr(self.data[mid], (self.metadata["key"])) < getattr(obj, (self.metadata["key"])):
+                start = mid + 1
+            else:
+                end = mid - 1
+        return start
+
+
+
+
---
+++ gui/model.py
@@ -0,0 +1,66 @@
+from PySide2 import QtWidgets, QtCore
+from gui.form import Form
+
+class Model(QtCore.QAbstractTableModel):
+    def __init__(self, data_list, parent=None):
+        super().__init__(parent)
+        self.data_list = data_list
+
+    def get_element(self, index):
+        return self.data_list.get_all()[index.row()]
+
+    def rowCount(self, index):
+        return len(self.data_list.get_all())
+
+    def columnCount(self, index):
+        return len(self.data_list.metadata["collumns"])
+
+    def data(self, index, role=QtCore.Qt.DisplayRole):
+        selected_data = self.get_element(index)
+        for i in range(len(self.data_list.metadata["collumns"])):
+            if index.column() == i and role == QtCore.Qt.DisplayRole:
+                return getattr(selected_data, (self.data_list.metadata["collumns"][i]))
+
+    def headerData(self, section, orientation, role=QtCore.Qt.DisplayRole):
+        for i in range(len(self.data_list.metadata["collumns"])):
+            if section == i and orientation == QtCore.Qt.Horizontal and role == QtCore.Qt.DisplayRole: # dodati proveru ako je collumns tipa list nemoj ga kreirati !!!!!
+                return self.data_list.metadata["collumns"][i].replace("_"," ").capitalize()
+
+    def setData(self, index, value, role=QtCore.Qt.EditRole):
+        self.selected_data = self.get_element(index)
+        if value == "":
+            return False
+        for i in range(len(self.data_list.metadata["collumns"])):
+            if index.column() == i and role == QtCore.Qt.EditRole:
+                if self.data_list.metadata["key"] == self.data_list.metadata["collumns"][i]:
+                    message_box = QtWidgets.QMessageBox()
+                    message_box.setWindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowTitleHint)
+                    message_box.setText("Kljuc nije moguce menjati!")
+                    message_box.exec()
+                    return False
+                self.data_list.edit(self.selected_data, self.data_list.metadata["collumns"][i], value)
+                return True
+        return False
+
+    def removeRows(self, row, rows, index=QtCore.QModelIndex()):
+        selected_data = self.get_element(index)
+        self.beginRemoveRows(QtCore.QModelIndex(), row, row + rows - 1)
+        self.data_list.delete_one(self.get_element(index))
+        self.endRemoveRows()
+        return True
+
+    def insertRows(self, row, rows, index=QtCore.QModelIndex()):
+        self.beginInsertRows(QtCore.QModelIndex(), row, row + rows-1)
+        forma = Form(self.data_list)
+        obj = forma.get_object()
+        if obj is None:
+            return False
+        self.data_list.insert(obj)
+        self.endInsertRows()
+        return True
+
+    def flags(self, index):
+        return super().flags(index) | QtCore.Qt.ItemIsEditable
+
+
+
