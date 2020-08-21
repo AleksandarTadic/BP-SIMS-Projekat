9ff30c90b506d654a7246e93934e09b751c30ade
Aleksandar <aleksandar.tadic98@gmail.com>
2020-08-21 18:33:36 +0200

    text serial file

--- database/handlers/serial_handler.py
+++ database/handlers/serial_handler.py
@@ -1,74 +1,78 @@
 import json
 import pickle
 
-
 class SerialHandler:
     def __init__(self, meta_filepath, filepath):
         super().__init__()
         self.filepath = "database/data/" + filepath
         self.meta_filepath = "database/metadata/" + meta_filepath
-        self.data = []
         self.metadata = {}
-        self.load_data()
+        self.load_metadata()
 
-    def load_data(self):
+    def load_metadata(self):
         try:
             with open(self.meta_filepath, "r") as meta_file:
                 self.metadata = json.load(meta_file)
         except FileNotFoundError:
             print("Meta file nije pronadjen!")
+
+    # def save_data(self):
+    #     with open(self.filepath, "w") as f:
+    #         for c_data in self.data:
+    #             f.writelines(str(" <|> ".join(c_data.values())) + '\n')
+
+    def get_all(self):
         try:
             with open(self.filepath, "r") as f:
                 lines = f.read().splitlines()
-                l_data = []
+                data = []
                 for d in lines:
-                    l_data.append(dict(zip(self.metadata["collumns"], list(d.split(" <|> ")))))
-                self.data = l_data
+                    data.append(self.to_dict(d))
+                return data
         except FileNotFoundError:
             print(self.filepath)
-            self.save_data()  
-
-    def save_data(self):
-        with open(self.filepath, "w") as f:
-            for c_data in self.data:
-                f.writelines(str(" <|> ".join(c_data.values())) + '\n')
-
-    def get_one(self, id):
-        return self.data[self.search[id]]
-
-    def get_all(self):
-        return self.data
 
     def insert(self, obj):
-        self.data.append(obj)
-        self.save_data()
-
-    def insert_many(self, obj_list):
-        if len(obj_list) > 0:
-            if not isinstance(obj_list, list):
-                return
-            for obj in obj_list:
-                self.insert(obj)
+        try:
+            with open(self.filepath, "a") as f:
+                f.write(self.to_text(obj))
+        except FileNotFoundError:
+            print(self.filepath)
 
     def edit(self, id, attr, value):
-        self.data[self.search(id)][attr] = value
-        self.save_data()
+        with open(self.filepath, "r+") as f:
+            lines = f.read().splitlines()
+            f.seek(0)
+            found = False
+            for ln in lines:
+                current_line = self.to_dict(ln)
+                if id == current_line and found == False:
+                    new_value= id
+                    new_value[attr] = value
+                    found = True
+                    f.write(self.to_text(new_value))
+                else:
+                    f.write(ln + '\n')
+            f.truncate()
 
     def delete_one(self, id):
-        self.data.remove(self.data[self.search(id)])
-        self.save_data()
+        with open(self.filepath, "r+") as f:
+            lines = f.read().splitlines()
+            f.seek(0)
+            deleted = False
+            for ln in lines:
+                current_line = self.to_dict(ln)
+                if id == current_line and deleted == False:
+                    deleted = True
+                    continue
+                f.write(ln + '\n')
+            f.truncate()
+            
+    def to_dict(self, line):
+        return dict(zip(self.metadata["collumns"], list(line.split(" <|> "))))
 
-    def search(self, id):
-        for d in range(len(self.data)):
-            if self.data[d] == id:
-                return d
-        return None
-
-    # def concat(self, keys):
-    #     primary_key = ""
-    #     for i in range(len(self.metadata["key"])):
-    #         primary_key += str(keys[self.metadata["key"][i]])
-    #     return primary_key
+    def to_text(self, current_dict):
+        return str(" <|> ".join(current_dict.values())) + '\n'
 
     def is_database(self):
         return False
